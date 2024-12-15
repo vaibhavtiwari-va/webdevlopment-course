3d4594dd9dd93e774c1c0b20c3e777826f820355
vaibhavtiwari-va <vaibhavtiwari010705@gmail.com>
2024-12-15 16:50:18 +0530

    Add files via upload

---
+++ new.js
@@ -0,0 +1,8 @@
+prompt("hello welcome to the website")
+
+const a= console.log("Enter the first number")
+const b=console.log("Enter the second number")
+const c=console.log("Enter your opration")
+
+
+
